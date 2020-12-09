a9005e77697d1bf18736d8332570231577572b31
FoLoKe <foloke@yandex.ru>
2020-12-09 13:42:56 +0300

    second lab complete

--- LAB_2/LAB_2_2/child_main.c
+++ LAB_2/LAB_2_2/child_main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World! %s\n", argv[0]);
+    printf("thread started %s %s\n", argv[0], argv[1]);
+    if(argc >= 2) {
+        FILE *file1 = fopen(argv[0], "r");
+        FILE *file2 = fopen(argv[1], "r");
+        if(file1 && file2) {
+            char a, b;
+            int same = 1;
+            int bytes = 0;
+            do {
+                a = getc(file1);
+                b = getc(file2);
+                if((a == EOF || b == EOF) && a == b) {
+                    break;
+                }
+                bytes += 2;
+                if (a == b) {
+                    continue;
+                } else {
+                    same = 0;
+                    break;
+                }
+            } while (a != EOF || b != EOF);
+
+            printf("files: %s and %s, pid: %i, bytes: %i ", argv[0], argv[1], getpid(), bytes);
+            if(same == 1) {
+                printf("files are same\n");
+            } else {
+                printf("files are different\n");
+            }
+
+            if(fclose(file1) == EOF || fclose(file2) == EOF) {
+                printf("close error\n");
+                return 2;
+            }
+        } else {
+            printf("cant open files\n");
+        }
+    }
     return 0;
 }
--- LAB_2/LAB_2_2/main.c
+++ LAB_2/LAB_2_2/main.c
@@ -5,37 +5,91 @@
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
+void sig_child(int signal);
+void startThread(char* path1, char* path2);
+int threadsCount;
 
 int main(int argc, char *argv[], char *envp[]) {
     if(argc >= 4) {
         DIR *dir1 = opendir(argv[1]);
         DIR *dir2 = opendir(argv[2]);
 
-        int maxThreads = atoi(argv[3]);
+        if (dir1 && dir2) {
 
-        printf("%s", "stared\n");
-        for(int i = 0; i<maxThreads; i++) {
-            char temp[20] = " ";
-            sprintf(temp, "%s %i", "thread ", i);
-            pid_t pid = fork();
+            int maxThreads = atoi(argv[3]);
+            threadsCount = 0;
 
-            if(pid == 0) {
-                if (execl("LAB_2_2_comparator", temp, "sasd", NULL) == -1) {
-                    char *errorbuf = strerror(errno);
-                    printf("%s", errorbuf);
+            printf("%s", "stared\n");
+            struct dirent *dirent1;
+            while ((dirent1 = readdir(dir1))) {
+                if(*(dirent1->d_name) == '.') {
+                    continue;
                 }
-            } else if (pid > 0) {
-                printf("created %s \n", temp);
-            } else {
-                printf("something went wrong i can feel it");
-            }
-        }
+                char newPath1[255] = "";//""
+                strcat(newPath1, argv[1]);
+                strcat(newPath1, "/");
+                strcat(newPath1, dirent1 -> d_name);
 
-        while (waitpid(-1, NULL, 0)) {
-            if(errno == ECHILD) {
-                break;
+                struct dirent *dirent2;
+                rewinddir(dir2);
+                while ((dirent2 = readdir(dir2))) {
+                    if(*(dirent2->d_name) == '.') {
+                        continue;
+                    }
+                    char newPath2[255] = "";//""
+                    strcat(newPath2, argv[2]);
+                    strcat(newPath2, "/");
+                    strcat(newPath2, dirent2 -> d_name);
+
+                    while (threadsCount >= maxThreads) {
+
+                    }
+
+                    startThread(newPath1, newPath2);
+                }
             }
+
+            if(closedir(dir1) == -1) {
+                printf("close error");
+            }
+            if(closedir(dir2) == -1) {
+                printf("close error");
+            }
+
+            while (threadsCount>0) {
+
+            }
+        } else {
+            printf("not a dirs");
         }
+    } else {
+        printf("too few arguments");
     }
     return 0;
+}
+
+void sig_child(int signal) {
+    pid_t pid;
+    int status;
+    printf("terminated, threads count: %i\n", threadsCount - 1);
+    threadsCount--;
+}
+
+void startThread(char* path1, char* path2) {
+    char temp[20] = " ";
+    printf("thread %s %s", path1, path2);
+    signal(SIGCHLD, sig_child);
+    threadsCount++;
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (execl("LAB_2_2_comparator", path1, path2, NULL) == -1) {
+            char *errorbuf = strerror(errno);
+            printf("%s", errorbuf);
+        }
+    } else if (pid > 0) {
+        printf(" created %s \n", temp);
+    } else {
+        printf("something went wrong i can feel it");
+    }
 }
